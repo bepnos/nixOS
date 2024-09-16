5d597872480589f47ee034a68fa5daaa6c51e604
bepnos <bepnos@proton.me>
2024-09-16 14:57:04 +0200

    [gen-147]: cleanup (2024-09-16 14:56:59)

--- hosts/desktop-home/home.nix
+++ hosts/desktop-home/home.nix
@@ -1,24 +1,6 @@
-{ pkgs, ... }: {
+{  ... }: {
   home = {
-    username = "bepnos";
-    homeDirectory = "/home/bepnos";
     stateVersion = "24.05";
-    sessionVariables.EDITOR = "vim";
-  };
-
-  nixpkgs.config.allowUnfree = true;
-  programs = {
-    home-manager.enable = true;
-    vscode = {
-      enable = true;
-      package = pkgs.vscode.fhs;
-      userSettings = {
-        "keyboard.dispatch" = "keyCode";
-        "nix.enableLanguageServer" = true;
-        "nix.serverPath" = "nixd";
-      };
-    };
-    btop.enable = true;
   };
   imports = [ ../../modules/home-manager ];
 }
---
+++ modules/home-manager/standard.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }: {
+  home = {
+    username = "bepnos";
+    homeDirectory = "/home/bepnos";
+    sessionVariables.EDITOR = "vim";
+  };
+
+  nixpkgs.config.allowUnfree = true;
+  programs = {
+    home-manager.enable = true;
+    vscode = {
+      enable = true;
+      package = pkgs.vscode.fhs;
+      userSettings = {
+        "keyboard.dispatch" = "keyCode";
+        "nix.enableLanguageServer" = true;
+        "nix.serverPath" = "nixd";
+        "nix.formatterPath" = "nixfmt";
+      };
+    };
+    btop.enable = true;
+  };
+ }
