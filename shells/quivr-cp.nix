9bdf9a71798d24138aabb8e8924e2fed8a5461d7
bepnos <bepnos@proton.me>
2024-09-17 11:27:38 +0200

    [gen-164]: cp shell (2024-09-17 10:21:22)

--- shells/quivr-cp.nix
+++ shells/quivr-cp.nix
@@ -1,18 +1,14 @@
-let pkgs = import <nixpkgs> {};
+let
+    pkgsnode = import (builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/7a339d87931bba829f68e94621536cad9132971a.tar.gz"; # Node 20.11.0
+    }) {};
+    pkgsyarn = import (builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/e08a8231e2c827f586e64727c1063c5e61dbc00d.tar.gz"; # yarn-berry 4.1.1
+    }) {};
+    pkgs = pkgsnode // pkgsyarn;
+    node = pkgs.elmPackages.nodejs;
+    yarn = pkgs.yarn-berry;
 
-    buildNodejs = pkgs.callPackage <nixpkgs/pkgs/development/web/nodejs/nodejs.nix> {};
-    
-    nodejs-20 = buildNodejs {
-      enableNpm = true;
-      version = "20.11.0";
-      sha256 = "";
-    };
-
-in pkgs.mkShell rec {
-  name = "quivr-cp";
-  
-  buildInputs = with pkgs; [
-    nodejs-20
-    yarn-berry
-  ];
+in pkgs.mkShell {
+  nativeBuildInputs = [node yarn];
 }
