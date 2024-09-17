7ad6db7941960a30efb41ee382e76447b1126963
bepnos <bepnos@proton.me>
2024-09-17 13:12:16 +0200

    [gen-164]: yarn (2024-09-17 10:21:22)

--- shells/quivr-cp.nix
+++ shells/quivr-cp.nix
@@ -3,7 +3,7 @@ let
         url = "https://github.com/NixOS/nixpkgs/archive/7a339d87931bba829f68e94621536cad9132971a.tar.gz"; # Node 20.11.0
     }) {};
     pkgsyarn = import (builtins.fetchTarball {
-        url = "https://github.com/NixOS/nixpkgs/archive/e08a8231e2c827f586e64727c1063c5e61dbc00d.tar.gz"; # yarn-berry 4.1.1
+        url = "https://github.com/yarnpkg/berry/archive/refs/tags/@yarnpkg/cli/4.4.1.tar.gz"; # yarn-berry 4.4.1
     }) {};
     pkgs = pkgsnode // pkgsyarn;
     node = pkgs.elmPackages.nodejs;
