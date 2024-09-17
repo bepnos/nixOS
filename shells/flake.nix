f4a459af4f9b99e5f6a2685adbc91ca17afc2cf8
bepnos <bepnos@proton.me>
2024-09-17 14:41:33 +0200

    [gen-164]: pin yarn-berry (2024-09-17 10:21:22)

--- shells/flake.nix
+++ shells/flake.nix
@@ -3,7 +3,8 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.05"; # Most shells will not need cutting edge nixpkgs
-    nodejs-cp.url = "github:nixos/nixpkgs/080a4a27f206d07724b88da096e27ef63401a504";
+    yarn-cp.url = "nixpkgs/a9858885e197f984d92d7fe64e9fff6b2e488d40#yarn-berry"; # 4.2.2
+    nodejs-cp.url = "github:nixos/nixpkgs/080a4a27f206d07724b88da096e27ef63401a504"; # 20.11.1
   };
 
   outputs = { self , nixpkgs, ... }@inputs: 
@@ -14,7 +15,7 @@
   {
     devShells."x86_64-linux".default = pkgs.mkShell { 
       nativeBuildInputs = with pkgs; [
-        yarn-berry
+        inputs.yarn-cp.legacyPackages.${system}.yarn-berry
         inputs.nodejs-cp.legacyPackages.${system}.nodejs_20
       ];
     };
