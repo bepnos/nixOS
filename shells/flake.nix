2dac9e2326a0988a32ab2f2795a7ffcaccf4ce74
bepnos <bepnos@proton.me>
2024-10-08 22:45:44 +0200

    [gen-22]: python shell (2024-10-08 21:52:32)

--- shells/flake.nix
+++ shells/flake.nix
@@ -7,17 +7,28 @@
     nodejs-cp.url = "nixpkgs/10b813040df67c4039086db0f6eaf65c536886c6"; # 20.11.0
   };
 
-  outputs = { self , nixpkgs, ... }@inputs: 
-  let 
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in
-  {
-    devShells."x86_64-linux".default = pkgs.mkShell { 
-      nativeBuildInputs = with pkgs; [
-        inputs.yarn-cp.legacyPackages.${system}.yarn-berry
-        inputs.nodejs-cp.legacyPackages.${system}.nodejs_20
-      ];
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells."x86_64-linux" = {
+        cp = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            inputs.yarn-cp.legacyPackages.${system}.yarn-berry
+            inputs.nodejs-cp.legacyPackages.${system}.nodejs_20
+          ];
+        };
+        python = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            python3
+            python312Packages.numpy
+            python312Packages.pandas
+            python312Packages.scipy
+          ];
+        };
+      };
     };
-  };
 }
