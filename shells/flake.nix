99a43812addd9e394cb5caf06de9e373b5d964ef
bepnos <bepnos@proton.me>
2024-10-28 13:18:32 +0100

    [gen-14]: cpp (2024-10-20 14:41:55)

--- shells/flake.nix
+++ shells/flake.nix
@@ -31,6 +31,20 @@
           ];
           VIRTUAL_ENV = "python";
         };
+        cpp = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            clang-tools
+            cmake
+            codespell
+            conan
+            cppcheck
+            doxygen
+            gtest
+            lcov
+            vcpkg
+            vcpkg-tool
+          ];
+        };
       };
     };
 }
