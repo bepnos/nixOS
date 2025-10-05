88973b94de160d1b57a2ec9923bd5545b3658f00
bepnos <bepnos@proton.me>
2025-10-05 19:37:10 +0200

    [gen-223]: gammastep (2025-10-05 19:37:05)

--- modules/home-manager/standard.nix
+++ modules/home-manager/standard.nix
@@ -9,4 +9,11 @@
   nixpkgs.config.allowUnfree = true;
   programs.home-manager.enable = true;
   programs.btop.enable = true;
+
+  services.gammastep = {
+    enable = true;
+    latitude = "51";
+    longitude = "4.7";
+    tray = true;
+  };
 }
